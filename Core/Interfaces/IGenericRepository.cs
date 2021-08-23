60e0ecad6d1de57dee6a20250b9f6ae65016f974
Safonov Kostyantyn <kotofey.web@gmail.com>
2021-08-23 13:01:41 +0300

    Added new functionality and DB Entities

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+   public abstract class IGenericRepository<T> where T : BaseEntity
+   {
+      public abstract Task<T> GetByIdAsync(int id);
+
+      public abstract Task<IReadOnlyList<T>> ListAllAsync();
+   }
+}
---
+++ Core/Specifications/ISpecification.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Core.Specifications
+{
+   public class ISpecification<T>
+   {
+      private Expression<Func<T, bool>> Criteria { get; }
+      private List<Expression<Func<T, object>>> Includes { get; }
+   }
+}
